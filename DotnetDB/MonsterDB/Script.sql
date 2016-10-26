89e51c173039c409e6c6ee12138b9e6df4148028
Richard Voelker <richard.voelker.software@gmail.com>
2016-10-26 12:02:06 -0400

    Add tables to MonsterDB.

--- DotnetDB/MonsterDB/Script.sql
+++ DotnetDB/MonsterDB/Script.sql
@@ -1,57 +1,57 @@
-﻿--CREATE SCHEMA Monster; -- A group of tables; like a namespace.
---GO
+﻿CREATE SCHEMA Monster; -- A group of tables; like a namespace.
+GO
 
---CREATE TABLE Monster.Monster (
---    MonsterId INT NOT NULL IDENTITY(1, 1), -- Identity is MS SQL's auto increment.
---    GenderId INT NULL,
---    TitleId INT NULL, -- nvarchar is the UTF-8 version of varchar (which is ASCII)
---    TypeId INT NOT NULL,
---    Name NVARCHAR(250) NOT NULL,
---    PicturePath NVARCHAR(256) NULL,
---    Active BIT NOT NULL
---);
---GO
+CREATE TABLE Monster.Monster (
+    MonsterId INT NOT NULL IDENTITY(1, 1), -- Identity is MS SQL's auto increment.
+    GenderId INT NULL,
+    TitleId INT NULL, -- nvarchar is the UTF-8 version of varchar (which is ASCII)
+    TypeId INT NOT NULL,
+    Name NVARCHAR(250) NOT NULL,
+    PicturePath NVARCHAR(256) NULL,
+    Active BIT NOT NULL
+);
+GO
 
---CREATE INDEX ix_monster_name
---    ON Monster.Monster (Name);
---GO
+CREATE INDEX ix_monster_name
+    ON Monster.Monster (Name);
+GO
 
---CREATE TABLE Monster.MonsterType (
---    MonsterTypeId INT NOT NULL IDENTITY(1, 1) PRIMARY KEY,
---    TypeName NVARCHAR(250) NOT NULL,
---    Active BIT NOT NULL
---);
---GO
+CREATE TABLE Monster.MonsterType (
+    MonsterTypeId INT NOT NULL IDENTITY(1, 1) PRIMARY KEY,
+    TypeName NVARCHAR(250) NOT NULL,
+    Active BIT NOT NULL
+);
+GO
 
---CREATE TABLE Monster.Gender (
---    GenderId INT NOT NULL IDENTITY(1, 1) PRIMARY KEY,
---    GenderName NVARCHAR(250) NOT NULL,
---    Active BIT NOT NULL
---);
---GO
+CREATE TABLE Monster.Gender (
+    GenderId INT NOT NULL IDENTITY(1, 1) PRIMARY KEY,
+    GenderName NVARCHAR(250) NOT NULL,
+    Active BIT NOT NULL
+);
+GO
 
---CREATE TABLE Monster.Title (
---    TitleId INT NOT NULL IDENTITY(1, 1) PRIMARY KEY,
---    TitleName NVARCHAR(25) NOT NULL,
---    Active BIT NOT NULL
---);
---GO
+CREATE TABLE Monster.Title (
+    TitleId INT NOT NULL IDENTITY(1, 1) PRIMARY KEY,
+    TitleName NVARCHAR(25) NOT NULL,
+    Active BIT NOT NULL
+);
+GO
 
---ALTER TABLE Monster.Monster
---    ADD CONSTRAINT pk_monster_monsterid PRIMARY KEY CLUSTERED (MonsterId); -- Clustered index is ordered (like dictionary). Non-clustered is like the index page.
---GO
+ALTER TABLE Monster.Monster
+    ADD CONSTRAINT pk_monster_monsterid PRIMARY KEY CLUSTERED (MonsterId); -- Clustered index is ordered (like dictionary). Non-clustered is like the index page.
+GO
 
---ALTER TABLE Monster.Monster
---    ADD CONSTRAINT fk_monster_genderid FOREIGN KEY (GenderId) REFERENCES Monster.Gender (GenderId);
---GO
+ALTER TABLE Monster.Monster
+    ADD CONSTRAINT fk_monster_genderid FOREIGN KEY (GenderId) REFERENCES Monster.Gender (GenderId);
+GO
 
---ALTER TABLE Monster.Monster
---    ADD CONSTRAINT fk_monster_titleid FOREIGN KEY (TitleId) REFERENCES Monster.Title (TitleId);
---GO
+ALTER TABLE Monster.Monster
+    ADD CONSTRAINT fk_monster_titleid FOREIGN KEY (TitleId) REFERENCES Monster.Title (TitleId);
+GO
 
---ALTER TABLE Monster.Monster
---    ADD CONSTRAINT fk_monster_typeid FOREIGN KEY (TypeId) REFERENCES Monster.MonsterType (MonsterTypeId);
---GO
+ALTER TABLE Monster.Monster
+    ADD CONSTRAINT fk_monster_typeid FOREIGN KEY (TypeId) REFERENCES Monster.MonsterType (MonsterTypeId);
+GO
 
 --CREATE SCHEMA Media;
 --GO
