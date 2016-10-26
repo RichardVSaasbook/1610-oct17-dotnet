444287b2847dc0a178a4ff69252088a39366ce2b
Richard Voelker <richard.voelker.software@gmail.com>
2016-10-26 16:20:23 -0400

    Add project data access.

---
+++ MonsterApp/MonsterApp.DataAccess/AdoData.cs
@@ -0,0 +1,108 @@
+﻿using MonsterApp.DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonsterApp.DataAccess
+{
+    /* 
+     * 2 types of modifiers:
+     *  - access modifiers
+     *    - private      |   Most restrictive
+     *    - protected    |
+     *    - internal    \ /                         // Works within the assembly; this is the default modifier.
+     *    - public       `   Least restrictive
+     *  - extended modifiers                        // Additional modifiers on top
+     *    - static      (class)
+     *    - sealed      (only class)
+     *    - virtual
+     *    - abstract    (class)
+     *    - new
+     *    - partial     (class)
+     *    - override
+     *    - readonly
+     *    - const
+     * 
+     * - Fields                                     // Variables
+     * - Constructors
+     * - Properties                                 // Variables with getter/setter methods
+     * - Methods
+     * - Functions                                  // Function can be passed to parameters of methods
+     * - Destructors
+     */
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class AdoData
+    {
+        private string connectionString = ConfigurationManager.ConnectionStrings["MonsterDB"].ConnectionString;
+
+        #region Methods
+        
+        /// <returns>A List of all Genders.</returns>
+        public List<Gender> GetGenders()
+        {
+            try
+            {
+                List<Gender> genders = new List<Gender>();
+                var ds = GetDataDisconnected("SELECT * FROM Monster.Gender;");
+
+                foreach (DataRow row in ds.Tables[0].Rows)
+                {
+                    genders.Add(new Gender
+                    {
+                        GenderId = int.Parse(row[0].ToString()),
+                        Name = row[1].ToString(),
+                        Active = bool.Parse(row[2].ToString())
+                    });
+                }
+
+                return genders;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+        
+        /// <returns>A List of all MonsterTypes.</returns>
+        public List<MonsterType> GetMonsterTypes()
+        {
+            throw new NotImplementedException("Todo.");
+        }
+        
+        /// <returns>A List of all Titles.</returns>
+        public List<Title> GetTitles()
+        {
+            throw new NotImplementedException("Todo.");
+        }
+
+        #endregion
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        private DataSet GetDataDisconnected(string query)
+        {
+            SqlDataAdapter da;
+            DataSet ds = new DataSet();
+            SqlCommand cmd;
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                cmd = new SqlCommand(query, connection);
+                da = new SqlDataAdapter(cmd);
+                da.Fill(ds);
+            }
+
+            return ds;
+        }
+    }
+}
---
+++ MonsterApp/MonsterApp.DataAccess/Models/MonsterType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonsterApp.DataAccess.Models
+{
+    /// <summary>
+    /// The type of monsters you can have in movies.
+    /// </summary>
+    public class MonsterType
+    {
+        public int MonsterTypeId { get; set; }
+
+        public string TypeName { get; set; }
+
+        public bool Active { get; set; }
+    }
+}
