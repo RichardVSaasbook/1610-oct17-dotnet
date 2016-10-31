508976ab20cda0b29847826a96c2edee208f7094
Richard Voelker <richard.voelker.software@gmail.com>
2016-10-31 12:03:44 -0400

    Add the service project.

---
+++ MonsterApp/MonsterApp.DataClient/IMonsterService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace MonsterApp.DataClient
+{
+    [ServiceContract]
+    public interface IMonsterService
+    {
+        [OperationContract]
+        string DoWork();
+
+        // void Menu(); // Part of the service contract, but not made available to the outside. (Need OperationContract to do).
+    }
+}
